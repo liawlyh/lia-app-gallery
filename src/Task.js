2b7bdb36da0feff13ce2722db75fdbcf87f4b1b5
liawlyh~ <liaawaliyah498@gmail.com>
2023-06-20 20:39:41 +0700

    mengerjakan netlify

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Lia Awaliyah" // TODO: replace this
+const studentId = "FE5589390" // TODO: replace this
+
+export { url, studentName, studentId }
